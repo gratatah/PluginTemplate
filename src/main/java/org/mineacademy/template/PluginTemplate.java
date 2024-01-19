7cdfa204145c31935d6471aa4a803902f40d4159
grat <gratatahyt@gmail.com>
2024-01-18 17:15:22 -0700

    Learned some new stuff
    Messed around with else if statements and operators. See onPluginStart{}

--- src/main/java/org/mineacademy/template/PluginTemplate.java
+++ src/main/java/org/mineacademy/template/PluginTemplate.java
@@ -20,6 +20,47 @@ public final class PluginTemplate extends SimplePlugin {
 	@Override
 	protected void onPluginStart() {
 
+		//int result = 4;
+		//System.out.println(result); //This spits out 4
+
+		//result = result + 5; //(using result as a reference)
+		//System.out.println(result); //This spits out 9
+
+		//result = 4 + 5;
+		//System.out.println(result); //This spits out 9
+
+		//System.out.println("Modulo: " + (15 % 4)); // 15/4 = 3.75 (take the 3 remove the .75) 3*4 = 12. 15-12 = 3
+
+		int apples = 5;
+		apples += 5; // adds 5
+		++apples; // adds 1
+		apples *= 4; // multiplies apples (11) by 4
+		System.out.println(apples); //44
+		apples /= 4; //divides it by 4
+		System.out.println(apples); //sout apples again (11)
+
+		int test = 999;
+		int test2 = 991;
+
+		if (test != test2) {
+			System.out.println("999 does not equal 991 lol");
+			System.out.println("test test test");
+		} else if (5 == 5) {
+			System.out.println("5 does equal 5");
+			System.out.println("test2 test2 test2");
+		} else {
+			System.out.println("999 equals 991 dont ask me how");
+			System.out.println("you should never see this message!!!");
+		}
+
+		boolean result1 = test == test2;
+		boolean result2 = test != test2;
+		boolean result3 = test > test2;
+		boolean result4 = test < test2;
+		boolean result5 = test >= test2;
+		boolean result6 = test <= test2;
+
+
 		// String testing123123 = "Hello";
 		// testing123123 = "another testing123123";
 
@@ -64,7 +105,7 @@ public final class PluginTemplate extends SimplePlugin {
 			event.getRightClicked().setGlowing(true);
 		// Squid
 		if (event.getRightClicked().getType() == EntityType.SQUID)
-			event.getRightClicked().setSilent(true);
+			event.getRightClicked().setGravity(false);
 
 	}
 
